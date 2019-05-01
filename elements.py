8662a6153e3cebb01f949f7c5c9065c887b7a1f7
Reijer <rcvanderzande@gmail.com>
2019-05-01 22:17:18 +0200

    need to add ownership of areas!

--- elements.py
+++ elements.py
@@ -27,11 +27,7 @@ class area(object):
 		'''moves a not moved unit from self to other'''
 		for unitindex in range(len(self.units)):
 			if not self.units[unitindex].notmoved() and isinstance(self.units[unitindex], unittype):
-				print str(other)
-				print other.units
 				other.units.append(self.units.pop(unitindex))
-				print str(other)
-				print other.units
 				break
 
 	def getfleets(self):
@@ -47,7 +43,6 @@ class sea(area):
 		super(sea, self).__init__(name, connection, owned)
 
 	def hasboats(self):
-		print "poep boat"
 		return len(self.getfleets()) > 0
 
 
@@ -57,7 +52,6 @@ class land(area):
 		super(land, self).__init__(name, connection, owned)
 
 	def hastanks(self):
-		print "poep tank"
 		return len(self.gettanks()) > 0
 	
 
@@ -322,71 +316,92 @@ class creategame(object):
 
 if __name__ == '__main__':
 	g = game()
-	print g
-	print "checking players"
-	print g.players
-	print
-	print "checking nations"
-	print g.nations
-	print
-	print "checking areas"
-	print g.areas
-	print
+	# print g
+	# print "checking players"
+	# print g.players
+	# print
+	# print "checking nations"
+	# print g.nations
+	# print
+	# print "checking areas"
+	# print g.areas
+	# print
 	for nation in g.nations:
-		print
-		print "working on ", nation
-		print
-		print "getting areas of nation"
-		print g.getareas(nation)
-		print "fleet update, pre build"
-		print g.getfleets(nation)
-		print "tank update, pre build"
-		print g.getarmies(nation)
-		print
-		print "building tanks and fleets in al possible places"
-		print
+		# print
+		# print "working on ", nation
+		# print
+		# print "getting areas of nation"
+		# print g.getareas(nation)
+		# print "fleet update, pre build"
+		# print g.getfleets(nation)
+		# print "tank update, pre build"
+		# print g.getarmies(nation)
+		# print
+		# print "building tanks and fleets in al possible places"
+		# print
 		for area in g.getareas(nation):
 			area.buildunit()
+		# print "fleet update, post build"
+		# print g.getfleets(nation)
+		# for area in g.getfleets(nation):
+		# 	print
+		# 	print "from {} all move options: {}".format(area, g.getareamoveoptions(area=area))
+		# 	print "from {} sea move options: {}".format(area, g.getareamoveoptions(area=area, areatype=sea))
+		# 	if len(g.getareamoveoptions(area=area, areatype=sea)) > 0:
+		# 		area.moveunit(choice(g.getareamoveoptions(area=area, areatype=sea)), unittype=boat)
+		# 		print "new locations:"
+		# 		print g.getfleets(nation)
+		# print
 		print "fleet update, post build"
 		print g.getfleets(nation)
 		for area in g.getfleets(nation):
 			print
 			print "from {} all move options: {}".format(area, g.getareamoveoptions(area=area))
-			print "from {} sea move options: {}".format(area, g.getareamoveoptions(area=area, areatype=sea))
+			print "from {} land move options: {}".format(area, g.getareamoveoptions(area=area, areatype=sea))
 			if len(g.getareamoveoptions(area=area, areatype=sea)) > 0:
 				area.moveunit(choice(g.getareamoveoptions(area=area, areatype=sea)), unittype=boat)
 				print "new locations:"
 				print g.getfleets(nation)
 		print
-		print "tank update, post build"
-		print g.getarmies(nation)
-		for area in g.getarmies(nation):
-			print
-			print "from {} all move options: {}".format(area, g.getareamoveoptions(area=area))
-			print "from {} land move options: {}".format(area, g.getareamoveoptions(area=area, areatype=land))
-			if len(g.getareamoveoptions(area=area, areatype=land)) > 0:
-				area.moveunit(choice(g.getareamoveoptions(area=area, areatype=land)), unittype=tank)
-				print "new locations:"
-				print g.getarmies(nation)
-		print
-		print "{} has {} million".format(nation, nation.getsaldo())
-		nation.changesaldo(30)
-		print "{} has {} million".format(nation, nation.getsaldo())
-		print "nation tries pays 31 million"
-		nation.changesaldo(-31)
-		print "{} has {} million".format(nation, nation.getsaldo())
-	print 
-	print "player stuff"
-	print
-	for player in g.players:
-		print "{} has {} million".format(player, player.getsaldo())
-		player.changesaldo(20)
-		print "{} has {} million".format(player, player.getsaldo())
-		player.changesaldo(-5)
-		print "{} has {} million".format(player, player.getsaldo())
-		print "player tries to pay 30 million"
-		player.changesaldo(-30)
-		print "{} has {} million".format(player, player.getsaldo())
+		for area in g.areas:
+			print area, area.units
+		for nation in g.nations:
+			print g.getfleets(nation)
+
+
+			# Units need to claim ownership of new terretories!
+
+
+
+	# 	print "tank update, post build"
+	# 	print g.getarmies(nation)
+	# 	for area in g.getarmies(nation):
+	# 		print
+	# 		print "from {} all move options: {}".format(area, g.getareamoveoptions(area=area))
+	# 		print "from {} land move options: {}".format(area, g.getareamoveoptions(area=area, areatype=land))
+	# 		if len(g.getareamoveoptions(area=area, areatype=land)) > 0:
+	# 			area.moveunit(choice(g.getareamoveoptions(area=area, areatype=land)), unittype=tank)
+	# 			print "new locations:"
+	# 			print g.getarmies(nation)
+	# 	print
+	# 	print "{} has {} million".format(nation, nation.getsaldo())
+	# 	nation.changesaldo(30)
+	# 	print "{} has {} million".format(nation, nation.getsaldo())
+	# 	print "nation tries pays 31 million"
+	# 	nation.changesaldo(-31)
+	# 	print "{} has {} million".format(nation, nation.getsaldo())
+	# print 
+	# print "player stuff"
+	# print
+	# for player in g.players:
+	# 	print "{} has {} million".format(player, player.getsaldo())
+	# 	player.changesaldo(20)
+	# 	print "{} has {} million".format(player, player.getsaldo())
+	# 	player.changesaldo(-5)
+	# 	print "{} has {} million".format(player, player.getsaldo())
+	# 	print "player tries to pay 30 million"
+	# 	player.changesaldo(-30)
+	# 	print "{} has {} million".format(player, player.getsaldo())
 
 
 
